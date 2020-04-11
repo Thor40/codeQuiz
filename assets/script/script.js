e0888037212d986b1f84b4d0ce8113b52aef0d1a
Thor40 <nicholas.kosik@gmail.com>
2020-04-11 13:11:39 -0500

    cycle q's and button add

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,14 +12,19 @@ game is over:
     save initials and score;
 SHOW HIGH SCORE;
 */
-var questionContent = document.querySelector("#question-content");
+var bodyEl = document.createElement("div");
 var btnStart = document.querySelector("#btn-start");
+var qtnContent = document.querySelector("#question-content");
 var welcomeContent = document.querySelector("#welcome-container");
-var questionIdCounter = 0;
+
 var timeLeft = 0;
 var timerEl = document.getElementById("timer");
+var questionChoices = [ "question one", "question two", "question three"];
+var questionIdCounter = 0;
+var answerChoices = [ "a", "b", "c", "d"];
+var answerIdCounter = 0;
 
-
+//timer
 function countdown() {
     timeLeft = 5;
 
@@ -32,63 +37,53 @@ function countdown() {
             clearInterval(timeInterval);
         }
     }, 1000);
+    //hide welcome elements
     document.getElementById("welcome-h1").style.display = 'none';
     document.getElementById("welcome-h2").style.display = 'none';
     document.getElementById("btn-start").style.display = 'none';
 };
 
+var createQuestions = function() {
+    var questionTitleEl = document.createElement("div");
+    counter = questionChoices[questionIdCounter]
+    
+    for (var i = 0; i < questionChoices.length; i++) {
+        questionTitleEl.innerHTML = "<h1 class='qtn-title'>" + counter + "</h1>";
+        questionIdCounter ++;
+    };
+    qtnContent.appendChild(questionTitleEl);
 
-
-var createQuestions = function(btnId) {
-    //create question
-    var qtnContainer = document.createElement('div');
-    qtnContainer.className = "qtn-content-div";
-
-    var questions = ["question 1", "question 2", "question 3"];
-    for (var i = 0; i < questions.length; i++) {
-        var questionOrderEl = document.createElement("h1");
-        questionOrderEl.textContent = questions[i];
-        questionOrderEl.setAttribute("value", questions[i]);
-        qtnContainer.appendChild(questionOrderEl);
-    }
-    questionContent.appendChild(qtnContainer);
 
     var btn1 = document.createElement('button');
-    btn1.setAttribute("id", btnId);
-    btn1.textContent = "one";
-    questionContent.appendChild(btn1);
+    btn1.setAttribute("id", "correctA");
+    btnCounter = answerChoices[0];
+    btn1.innerHTML = btnCounter;
+    qtnContent.appendChild(btn1);
 
     var btn2 = document.createElement('button');
-    btn2.setAttribute("id", btnId);
-    btn2.textContent = "two";
-    questionContent.appendChild(btn2);
+    btnCounter = answerChoices[1];
+    btn2.innerHTML = btnCounter;
+    qtnContent.appendChild(btn2);
 
     var btn3 = document.createElement('button');
-    btn3.setAttribute("id", btnId);
-    btn3.textContent = "three";
-    questionContent.appendChild(btn3);
+    btnCounter = answerChoices[2];
+    btn3.innerHTML = btnCounter;
+    qtnContent.appendChild(btn3);
 
     var btn4 = document.createElement('button');
-    btn4.setAttribute("id", btnId);
-    btn4.textContent = "four";
-    questionContent.appendChild(btn4);
+    btnCounter = answerChoices[3];
+    btn4.innerHTML = btnCounter;
+    qtnContent.appendChild(btn4);
+
+document.getElementById('correctA').addEventListener("click", answered)
+
 
 };
 
+var answered = function () {
+    alert("Correct!");
+};
 
-// create all elements
-// 
-// var btn2 = document.createElement('button');
-// var btn3 = document.createElement('button');
-// var btn4 = document.createElement('button');
-// // store button elements in variable
-// var buttonItems = document.getElementsByTagName('button');
-// // set text content for elements
-// // Append all of our elements
-// body.appendChild(infoEl);
-
-// set style attributes of all elements
-//infoEl.setAttribute('style', 'margin:auto; border:solid; padding-top:300px; padding-bottom:0px;' );
 
 document.getElementById("btn-start").addEventListener("click", countdown);
 document.getElementById("btn-start").addEventListener("click", createQuestions);
