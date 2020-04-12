a0e2f33cc3e039775a0fdb3c35d459b1f5e49b6f
Thor40 <nicholas.kosik@gmail.com>
2020-04-11 22:22:56 -0500

    different approach to question generation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,92 +1,168 @@
-/* 
-Click start button:
-    timer starts
-    presented with question;
-answer the question:
-    presented with another question;
-question answred incorrectly:
-    time subtracted from clock;
-all Q's answered or timer = 0:
-    gave is over;
-game is over:
-    save initials and score;
-SHOW HIGH SCORE;
-*/
-var bodyEl = document.createElement("div");
-var btnStart = document.querySelector("#btn-start");
-var qtnContent = document.querySelector("#question-content");
-var welcomeContent = document.querySelector("#welcome-container");
-
-var timeLeft = 0;
+var startButton = document.getElementById('btn-start')
+var nextButton = document.getElementById('btn-next')
+var questionContainerEl = document.getElementById('question-container')
+var welcome = document.getElementById('welcome-container')
+var questionEl = document.getElementById('question')
+const answerButtonEl = document.getElementById('btn-answer')
 var timerEl = document.getElementById("timer");
-var questionChoices = [ "question one", "question two", "question three"];
-var questionIdCounter = 0;
-var answerChoices = [ "a", "b", "c", "d"];
-var answerIdCounter = 0;
+var showScore = document.getElementById('btn-score')
+var scoreForm = document.getElementById('score-form')
+var countCorrectAnswers = 0;
+
+startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', countdown)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+showScore.addEventListener('click', highScore)
 
 //timer
 function countdown() {
-    timeLeft = 5;
+    timeLeft = 75;
 
     var timeInterval = setInterval(function() {
         timerEl.textContent = timeLeft;
         timeLeft --;
 
-        if (timeLeft === 0) {
+        if (timeLeft === 0 || shuffleQuestions.length > currentQuestionIndex + 1) {
             timerEl.textContent = "DONE"
             clearInterval(timeInterval);
         }
     }, 1000);
-    //hide welcome elements
-    document.getElementById("welcome-h1").style.display = 'none';
-    document.getElementById("welcome-h2").style.display = 'none';
-    document.getElementById("btn-start").style.display = 'none';
 };
 
-var createQuestions = function() {
-    var questionTitleEl = document.createElement("div");
-    question = questionChoices[questionIdCounter]
-    questionTitleEl.innerHTML = "<h1 class='qtn-title'>" + question + "</h1>";
 
-    qtnContent.appendChild(questionTitleEl);
+//index for question order/ random question shuffle
+var shuffleQuestions, currentQuestionIndex
 
+function startGame() {
+    // hiding start button
+    startButton.classList.add('hide')
+    welcome.classList.add('hide')
+    //randomly shuffle order of questions
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    // index of questions, starting on 0
+    currentQuestionIndex = 0
+    // unhiding questions and answers
+    questionContainerEl.classList.remove('hide')
+    // run next question function
+    setNextQuestion()
+    countCorrectAnswers = 0;
 
-    var btn1 = document.createElement('button');
-    btn1.setAttribute("id", "correctA");
-    btnCounter = answerChoices[answerIdCounter];
-    btn1.innerHTML = btnCounter;
-    if (answerIdCounter < 4) {
-        qtnContent.appendChild(btn1);
-        answerIdCounter++;
+}
+// sets next question
+function setNextQuestion () {
+    resetState()
+    // shuffles question per the current index location
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+
+}
+
+//show answers
+function showQuestion(question) {
+    // add the text to the current question
+    questionEl.innerText = question.question
+    //for each indiviudal answer
+    question.answers.forEach(answer => {
+        //create a button
+        var button = document.createElement('button')
+        // set the button text to answer
+        button.innerText = answer.text
+        // add class of btn
+        button.classList.add('btn')
+        //add a dataset attribute to button el
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        // take the button click in as a parameter
+        button.addEventListener('click', selectAnswer)
+        //append newly created button to the answer button element
+        answerButtonEl.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    //if there is a child in the answer button elements, remove
+    while (answerButtonEl.firstChild) {
+        answerButtonEl.removeChild(answerButtonEl.firstChild)
     }
-    
+}
 
-    // var btn2 = document.createElement('button');
-    // btnCounter = answerChoices[1];
-    // btn2.innerHTML = btnCounter;
-    // qtnContent.appendChild(btn2);
+// answer function set to target
+function selectAnswer (event) {
+    var selectedButton = event.target
+    // correct is pulled from dataset
+    var correct = selectedButton.dataset.correct
+    //if target is correct (see setStatusClass)
+    setStatusClass(document.body, correct)
+    // then it will populate a new set of questions from the next dataset
+    Array.from(answerButtonEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    //if we are at the last question, hide next to stop question cycling
+    if (shuffleQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+        // allow restart of game
+    } else {
+        showScore.classList.remove('hide')
+        showScore.innerText = "Show Score!"
+        questionContainerEl.classList.add('hide')
+    }
+    if (selectedButton.dataset = correct) {
+        countCorrectAnswers++;
+    }
+    localStorage.setItem('score', countCorrectAnswers);
+    document.getElementById('right-answers').innerHTML = countCorrectAnswers;
+}
 
-    // var btn3 = document.createElement('button');
-    // btnCounter = answerChoices[2];
-    // btn3.innerHTML = btnCounter;
-    // qtnContent.appendChild(btn3);
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
 
-    // var btn4 = document.createElement('button');
-    // btnCounter = answerChoices[3];
-    // btn4.innerHTML = btnCounter;
-    // qtnContent.appendChild(btn4);
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
-document.getElementById('correctA').addEventListener("click", answered)
+function highScore() {
+    localStorage.getItem(countCorrectAnswers)
+    questionContainerEl.classList.remove('hide')
+    showScore.classList.add('hide')
+    resetState()
+    var scoreInput = window.prompt("Please add initials!")
+    questionEl.innerText.add(scoreInput[countCorrectAnswers])
+}
 
-
-};
-
-var answered = function () {
-    alert("Correct!");
-    questionIdCounter++;
-    createQuestions();
-};
-
-
-document.getElementById("btn-start").addEventListener("click", countdown);
-document.getElementById("btn-start").addEventListener("click", createQuestions);
+// questions string and answers array
+var questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true, wrong: false},
+            { text: '2', correct: false, wrong: true },
+            { text: '7', correct: false, wrong: true },
+            { text: '5', correct: false, wrong: true }
+        ]
+    },
+    {
+        question: 'What is 4 + 3',
+        answers: [
+            { text: '7', correct: true },
+            { text: '56', correct: false }
+        ]
+    },
+    {
+        question: 'What color is the sky',
+        answers: [
+            { text: 'blue', correct: true },
+            { text: 'red', correct: false }
+        ]
+    }
+]
